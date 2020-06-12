e6c136652d61a0c1c78ce34f19f7bf227e13a64b
Than Nguyenvu <thelensguy@gmail.com>
2020-06-12 16:51:49 -0700

    Used HostBinding to Bind to Host Properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -4,12 +4,14 @@ import {
   OnInit,
   ElementRef,
   HostListener,
+  HostBinding,
 } from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighlight]",
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding("style.backgroundColor") backgroundColor: string = "transparent";
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
